e9d67ceab18f06fc4f7029fcd081bf21dc835176
Ollie Burke <ollie.burke@l2it.in2p3.fr>
2023-06-19 18:26:27 +0200

    using astropy, accurate

--- new_directory/Ollie_Codes/Estm_H0_MCMC.py
+++ new_directory/Ollie_Codes/Estm_H0_MCMC.py
@@ -3,30 +3,41 @@ import matplotlib.pyplot as plt
 from MCMC_func import MCMC_run
 from astropy.cosmology import FlatLambdaCDM
 
-H0_true = 70.5
-cosmo = FlatLambdaCDM(H0=H0_true, Om0=0.274, Ob0 = 0.046)
+H0_true = 70.5   # True value of hubble constant 
+Omega_m = 0.274
+Omega_Lambda = 1 - Omega_m
+c = 299792458.0 / 1e3
+z_s = 1.0 
 
-# Check the result: 
-z_s = 0.8
+kwargs = {'c':3e5, 'H0_true':H0_true, 'Omega_m':Omega_m, 
+            'Omega_Lambda':Omega_Lambda, 'z_s':z_s}
+# Assign a cosmology
+cosmo = FlatLambdaCDM(H0=H0_true, Om0=Omega_m)
 
+# Assume known redshift of source  
+
+# Calculate luminosity distance of source assuming a cosmology
 D_Mpc = cosmo.luminosity_distance(z_s).value      # Measure in Mpc
-delta_D_Mpc = (1/10) * 1e3
+# Give rough estimate of precision in parameters
+delta_D_Mpc = (1/30) * 1e3
 
-data_dl  = np.array([0, D_Mpc])
-delta_dl = np.array([1e-10, delta_D_Mpc])
-data_z   = np.array([0,z_s])
+# Input data
+data_dl  = np.array([D_Mpc])
+delta_dl = np.array([delta_D_Mpc])
+data_z   = np.array([z_s])
 
-param_start =[60]
-Ntotal = 100000 # Total number of iterations
-burnin = 30000  # Set burn-in. This is the amount of samples we will discard whilst looking 
-                # for the true parameters
-H0_var_prop = 0.8743905288970055
-printerval = 50000
+# Start initial H0 
+param_start =[H0_true]
 
-N_obs = len(data_z)
+# Run MCMC algorithm
+H0_chain, lp = MCMC_run(data_dl, delta_dl, param_start, **kwargs)
 
-H0_chain, lp = MCMC_run(data_dl, data_z, delta_dl, param_start)
+# Output results
+ma = np.mean(H0_chain)
+sda = np.std(H0_chain)
 
+print("mean of H0 =", ma, '_', "standard deviation of H0 =", sda)
+# Plot result
 plt.hist(H0_chain, bins = 45, color = 'turquoise', edgecolor = 'darkcyan', alpha = 1, label = 'Posterior')
 plt.xlabel(r'$H_{0}$',fontsize = 16)
 plt.ylabel(r'$P(H_{0}|d)$',fontsize = 16)
@@ -35,8 +46,4 @@ plt.axvline(x = H0_true, label = 'True value', c = 'red', linestyle = 'dashed')
 plt.legend()
 plt.tight_layout()
 plt.show() 
-ma = np.mean(H0_chain)
-sda = np.std(H0_chain)
-
-print("mean of H0 =", ma, '_', "standard deviation of H0 =", sda)
 
